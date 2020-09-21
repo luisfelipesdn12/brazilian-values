ee426aa2a71a2fb481b4bd244f9c7a37e5982ed4
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-09-21 02:19:08 -0300

    🏗 add Rollup.js bundler & define its options

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+// @ts-check
+
+import { terser } from 'rollup-plugin-terser';
+
+/**
+ * Creates an object with Rollup.js output options.
+ * @param {import('rollup').OutputOptions} options
+ * @returns {import('rollup').OutputOptions}
+ */
+function createOutputOption(options) {
+  return {
+    exports: 'named',
+    sourcemap: true,
+    ...options,
+  };
+}
+
+/**
+ * An object with Rollup.js options.
+ * @type {import('rollup').RollupOptions}
+ */
+const options = {
+  input: './src/brazilian-values.js',
+  output: [
+    createOutputOption({
+      file: './dist/brazilian-values.js',
+      format: 'commonjs',
+    }),
+    createOutputOption({
+      file: './dist/brazilian-values.esm.js',
+      format: 'esm',
+    }),
+    createOutputOption({
+      file: './dist/brazilian-values.umd.js',
+      name: 'BrazilianValues',
+      format: 'umd',
+    }),
+    createOutputOption({
+      file: './dist/brazilian-values.umd.min.js',
+      name: 'BrazilianValues',
+      format: 'umd',
+      plugins: [terser()],
+    }),
+  ],
+};
+
+export default options;
