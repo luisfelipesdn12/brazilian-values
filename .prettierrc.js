6b0ac910a2f925e876db65e0a03781cfb4f55bc7
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-09-21 02:16:41 -0300

    🏗 add Prettier.js as code formatter

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+/**
+ * An object with Prettier.js options.
+ * @type {import('prettier').Options}
+ */
+const options = {
+  arrowParens: 'always',
+  printWidth: 100,
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'all',
+};
+
+module.exports = options;
